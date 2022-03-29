70df0607dbfb8418cc70988d3274f43290dc2024
aldi7id <aldi7.indo@gmail.com>
2022-03-29 14:55:59 +0700

    add dialog standard and edit layout

--- app/src/main/java/com/ajgroup/dialog/MainActivity.kt
+++ app/src/main/java/com/ajgroup/dialog/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ajgroup.dialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.ajgroup.dialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Dialog Cancelable")
+        dialog.setMessage("Dialog cancelagle bisa ditutup dengan klik bagian luar dialog")
+        dialog.setCancelable(true)
+        dialog.show()
     }
 }
