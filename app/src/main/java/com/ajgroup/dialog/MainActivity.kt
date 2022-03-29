5b19d974b5d5608b017e415ab98f26af5f6e0359
aldi7id <aldi7.indo@gmail.com>
2022-03-29 15:07:24 +0700

    add alert dialog with button

--- app/src/main/java/com/ajgroup/dialog/MainActivity.kt
+++ app/src/main/java/com/ajgroup/dialog/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ajgroup.dialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.ajgroup.dialog.databinding.ActivityMainBinding
 
@@ -12,10 +13,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dialog = AlertDialog.Builder(this)
-        dialog.setTitle("Dialog Cancelable")
-        dialog.setMessage("Dialog cancelagle bisa ditutup dengan klik bagian luar dialog")
-        dialog.setCancelable(true)
-        dialog.show()
+        binding.btnDialogStandard.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("Dialog Cancelable")
+            dialog.setMessage("Dialog cancelagle bisa ditutup dengan klik bagian luar dialog")
+            dialog.setCancelable(true)
+            dialog.show()
+        }
+        binding.btnDialogButton.setOnClickListener {
+            val dialogWithButton = AlertDialog.Builder(this)
+            dialogWithButton.setTitle("Dialog Dengan Button")
+            dialogWithButton.setMessage("Dialog Dengan Button untuk berbagai Aksi")
+            dialogWithButton.setIcon(R.mipmap.ic_launcher_round)
+            
+            dialogWithButton.setCancelable(false)
+            dialogWithButton.setPositiveButton("Positive Button"){
+                dialogInterface, p1 ->
+                Toast.makeText(this, "Positif Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+            dialogWithButton.setNegativeButton("Negative Button"){
+                dialogInterface, p1->
+                Toast.makeText(this, "Negative Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+            dialogWithButton.setNeutralButton("Neutral Button"){
+                dialogInterface, p1 ->
+                Toast.makeText(this, "Neutral Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+        dialogWithButton.show()
+
+        }
     }
 }
